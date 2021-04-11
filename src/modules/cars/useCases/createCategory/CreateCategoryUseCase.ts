c3185c830a1930232712ebf051a2c930a9271fea
Jairo Koning <jairo.koning@gmail.com>
2021-04-11 14:59:14 -0300

    feat: errors treatment

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from 'tsyringe';
 
+import { AppError } from '../../../../errors/AppError';
 import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
 
 interface IRequest {
@@ -20,7 +21,7 @@ class CreateCategoryUseCase {
     );
 
     if (categoryAlreadyExists) {
-      throw new Error('Category already exist!s');
+      throw new AppError('Category already exist!s');
     }
 
     this.categoriesRepository.create({ name, description });
