f81744a2d343ea87e337720d2c4376896cc4d119
Jairo Koning <jairo.koning@gmail.com>
2021-04-13 21:20:26 -0300

    chore: install and configure jest

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,15 @@
+describe('Create Category', () => {
+  it('2 + 2 should be 4', () => {
+    const sum = 2 + 2;
+    const result = 4;
+
+    expect(sum).toBe(result);
+  });
+
+  it('2 + 2 should not be 5', () => {
+    const sum = 2 + 2;
+    const result = 5;
+
+    expect(sum).not.toBe(result);
+  });
+});
