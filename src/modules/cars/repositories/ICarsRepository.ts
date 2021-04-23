2b276f9ed3033f7b740e2f669b622e2adc64e91b
Jairo Koning <jairo.koning@gmail.com>
2021-04-22 21:45:31 -0300

    feat: TDD list cars

--- src/modules/cars/repositories/ICarsRepository.ts
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -1,9 +1,15 @@
 import { ICreateCarDTO } from '../dtos/ICreateCarDTO';
 import { Car } from '../infra/typeorm/entities/Car';
+import { Category } from '../infra/typeorm/entities/Category';
 
 interface ICarsRepository {
   create(data: ICreateCarDTO): Promise<Car>;
   findByLicensePlate(license_plate: string): Promise<Car>;
+  findAvailable(
+    brand?: string,
+    category_id?: string,
+    name?: string
+  ): Promise<Car[]>;
 }
 
 export { ICarsRepository };
