1ca6a3bbd1ba9df5bf0d444f0d02e4eccb3608e6
Jairo Koning <jairo.koning@gmail.com>
2021-04-30 07:14:32 -0300

    feat(rental): migrations

--- src/shared/infra/typeorm/migrations/1617869803711-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1617869803711-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1617869803711 implements MigrationInterface {
           {
             name: 'id',
             type: 'uuid',
+            isPrimary: true,
           },
           {
             name: 'name',
