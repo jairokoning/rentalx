1916a92823ed365f404ec02d0e305de1fc188197
Jairo Koning <jairo.koning@gmail.com>
2021-05-01 15:59:48 -0300

    feat(rental): TDD

---
+++ src/modules/rentals/useCases/createRental/createRentalUseCase.ts
@@ -0,0 +1,35 @@
+import { IRentalsRepository } from '@modules/rentals/repositories/IRentalsRepository';
+
+interface IRequest {
+  user_id: string;
+  car_id: string;
+  expected_return_date: Date;
+}
+
+class CreateRentalUseCase {
+  constructor(private rentalsRepository: IRentalsRepository) {}
+
+  async execute({
+    user_id,
+    car_id,
+    expected_return_date,
+  }: IRequest): Promise<void> {
+    const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(
+      car_id
+    );
+
+    if (carUnavailable) {
+      throw new Error('Car is unavailable!');
+    }
+
+    const rentalOpenToUser = await this.rentalsRepository.findOpenRentalByUser(
+      user_id
+    );
+
+    if (rentalOpenToUser) {
+      throw new Error('There is a rental in progres for user!');
+    }
+  }
+}
+
+export { CreateRentalUseCase };
