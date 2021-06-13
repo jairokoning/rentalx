dadb9a37c268f7202c5708a43403c982dca34392
Jairo Koning <jairo.koning@gmail.com>
2021-06-13 15:04:28 -0300

    feat(forgot password): usecase controller

---
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -0,0 +1,38 @@
+import { inject } from 'tsyringe';
+import { v4 as uuidV4 } from 'uuid';
+
+import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
+import { IUsersTokensRepository } from '@modules/accounts/repositories/IUsersTokensRepository';
+import { IDateProvider } from '@shared/container/providers/DateProvider/IDateProvider';
+import { AppError } from '@shared/errors/AppError';
+
+class SendForgotPasswordMailUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: UsersRepository,
+    @inject('UsersTokensRepository')
+    private usersTokensRepository: IUsersTokensRepository,
+    @inject('DayjsDateProvider')
+    private dateProvider: IDateProvider
+  ) {}
+
+  async execute(email: string) {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('Users does not exists!');
+    }
+
+    const token = uuidV4();
+
+    const expires_date = this.dateProvider.addHours(3);
+
+    await this.usersTokensRepository.create({
+      refresh_token: token,
+      user_id: user.id,
+      expires_date,
+    });
+  }
+}
+
+export { SendForgotPasswordMailUseCase };
